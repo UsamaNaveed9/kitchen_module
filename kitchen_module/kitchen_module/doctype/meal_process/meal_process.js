93e162c299f90b23e8ad7026dfe3bc646654c32e
usamanaveed9 <usamanaveed9263@gmail.com>
2022-09-17 12:03:01 +0000

    fect:some fields added in meal process doctype and stock entry generated seperatly for finished item

--- kitchen_module/kitchen_module/doctype/meal_process/meal_process.js
+++ kitchen_module/kitchen_module/doctype/meal_process/meal_process.js
@@ -58,132 +58,138 @@ frappe.ui.form.on('Meal Process', {
 		})
 	},
 	get_items: function(frm){
-		if(cur_frm.doc.get_items_from == "Sales Order"){
-			let sales_orders = cur_frm.doc.sales_orders;
-			for(let s=0; s<sales_orders.length; s++){
-			frappe.db.get_doc('Sales Order', sales_orders[s].sales_order)
-    			.then(d => {
-        				//console.log(d.items);
-						for (let i = 0; i < d.items.length; i++){
-							let main_item = cur_frm.add_child("main_items");
-							main_item.item_code = d.items[i].item_code;
-							main_item.item_name = d.items[i].item_name;
-							main_item.qty = d.items[i].qty;
-							main_item.uom = d.items[i].uom;
-							main_item.rate = d.items[i].rate;
-							main_item.amount = d.items[i].amount;
-							main_item.reference_doc = sales_orders[s].sales_order
-							cur_frm.refresh_field("main_items");
-
-							frappe.db.get_value('BOM', {item: d.items[i].item_code, is_default: '1'}, ['name','total_cost'])
-    						.then(r => {
-        							let values = r.message;
-        							//console.log(values);
-									let t_bom = cur_frm.add_child("bom_list");
-									t_bom.bom = values.name;
-									t_bom.cost_per_unit = values.total_cost
-									t_bom.reference_doc = sales_orders[s].sales_order
-									t_bom.main_item = d.items[i].item_code;
-									cur_frm.refresh_field("bom_list");
-
-									frappe.db.get_doc('BOM', values.name)
-    									.then(b => {
-        									//console.log(b.items);
-											for (let j = 0; j < b.items.length; j++){
-												let recipe_item = cur_frm.add_child("recipe_items");
-												recipe_item.item_code = b.items[j].item_code;
-												recipe_item.item_name = b.items[j].item_name;
-												recipe_item.qty = b.items[j].qty;
-												recipe_item.uom = b.items[j].uom;
-												frappe.db.get_value("Bin", {"item_code":b.items[j].item_code, "warehouse":cur_frm.doc.source_warehouse}, "actual_qty").then((r)=>{
-													if(r.message.actual_qty){
-														recipe_item.available_qty = r.message.actual_qty;
-													}
-												cur_frm.refresh_field("recipe_items");
-												})
-												frappe.db.get_value("Bin", {"item_code":b.items[j].item_code}, "valuation_rate").then((r)=>{
-													if(r.message.valuation_rate){
-														let price = 0;
-														price = r.message.valuation_rate
-														recipe_item.rate = price;
-														recipe_item.amount = b.items[j].qty * price;
-													}
-												cur_frm.refresh_field("recipe_items");
-												})
-												recipe_item.parent_item = d.items[i].item_code;
-												recipe_item.bom = values.name;
-												recipe_item.reference_doc = sales_orders[s].sales_order
-											}
-									cur_frm.refresh_field("recipe_items");
-    								})
-    						})
-						}		
-    			})
+		if(cur_frm.doc.source_warehouse){
+			if(cur_frm.doc.get_items_from == "Sales Order"){
+				frm.clear_table("main_items");
+				frm.clear_table("bom_list");
+				frm.clear_table("recipe_items");
+				let sales_orders = cur_frm.doc.sales_orders;
+				for(let s=0; s<sales_orders.length; s++){
+				frappe.db.get_doc('Sales Order', sales_orders[s].sales_order)
+					.then(d => {
+							//console.log(d.items);
+							for (let i = 0; i < d.items.length; i++){
+								let main_item = cur_frm.add_child("main_items");
+								main_item.item_code = d.items[i].item_code;
+								main_item.item_name = d.items[i].item_name;
+								main_item.qty = d.items[i].qty;
+								main_item.uom = d.items[i].uom;
+								main_item.rate = d.items[i].rate;
+								main_item.amount = d.items[i].amount;
+								main_item.seles_order_ref = sales_orders[s].sales_order;
+								cur_frm.refresh_field("main_items");
+	
+								frappe.db.get_value('BOM', {item: d.items[i].item_code, is_default: '1'}, ['name','total_cost'])
+								.then(r => {
+										let values = r.message;
+										//console.log(values);
+										let t_bom = cur_frm.add_child("bom_list");
+										t_bom.bom = values.name;
+										t_bom.main_item = d.items[i].item_code;
+										t_bom.cost_per_unit = values.total_cost;
+										t_bom.sales_order_ref = sales_orders[s].sales_order;
+										cur_frm.refresh_field("bom_list");
+	
+										frappe.db.get_doc('BOM', values.name)
+											.then(b => {
+												//console.log(b.items);
+												for (let j = 0; j < b.items.length; j++){
+													let recipe_item = cur_frm.add_child("recipe_items");
+													recipe_item.item_code = b.items[j].item_code;
+													recipe_item.item_name = b.items[j].item_name;
+													recipe_item.qty = b.items[j].qty;
+													recipe_item.uom = b.items[j].uom;
+													frappe.db.get_value("Bin", {"item_code":b.items[j].item_code, "warehouse":cur_frm.doc.source_warehouse}, ["actual_qty","valuation_rate"]).then((r)=>{
+														if(r.message.actual_qty){
+															recipe_item.available_qty = r.message.actual_qty;
+														}
+														if(r.message.valuation_rate){
+															let price = 0;
+															price = r.message.valuation_rate
+															recipe_item.rate = price;
+															recipe_item.amount = b.items[j].qty * price;
+														}
+													cur_frm.refresh_field("recipe_items");
+													})
+													recipe_item.parent_item = d.items[i].item_code;
+													recipe_item.bom = values.name;
+													recipe_item.sales_order_ref = sales_orders[s].sales_order;
+												}
+										cur_frm.refresh_field("recipe_items");
+										})
+								})
+							}		
+					})
+				}
+			}
+			else{
+				frm.clear_table("main_items");
+				frm.clear_table("bom_list");
+				frm.clear_table("recipe_items");
+				let material_requests = cur_frm.doc.material_requests;
+				for(let s=0; s<material_requests.length; s++){
+				frappe.db.get_doc('Material Request', material_requests[s].material_request)
+					.then(d => {
+							//console.log(d.items);
+							for (let i = 0; i < d.items.length; i++){
+								let main_item = cur_frm.add_child("main_items");
+								main_item.item_code = d.items[i].item_code;
+								main_item.item_name = d.items[i].item_name;
+								main_item.qty = d.items[i].qty;
+								main_item.uom = d.items[i].uom;
+								main_item.rate = d.items[i].rate;
+								main_item.amount = d.items[i].amount;
+								main_item.material_request_ref = material_requests[s].material_request;
+								cur_frm.refresh_field("main_items");
+	
+								frappe.db.get_value('BOM', {item: d.items[i].item_code, is_default: '1'}, ['name','total_cost'])
+								.then(r => {
+										let values = r.message;
+										//console.log(values);
+										let t_bom = cur_frm.add_child("bom_list");
+										t_bom.bom = values.name;
+										t_bom.main_item = d.items[i].item_code;
+										t_bom.cost_per_unit = values.total_cost
+										t_bom.material_request_ref = material_requests[s].material_request;
+										cur_frm.refresh_field("bom_list");
+	
+										frappe.db.get_doc('BOM', values.name)
+											.then(b => {
+												//console.log(b.items);
+												for (let j = 0; j < b.items.length; j++){
+													let recipe_item = cur_frm.add_child("recipe_items");
+													recipe_item.item_code = b.items[j].item_code;
+													recipe_item.item_name = b.items[j].item_name;
+													recipe_item.qty = b.items[j].qty;
+													recipe_item.uom = b.items[j].uom;
+													frappe.db.get_value("Bin", {"item_code":b.items[j].item_code, "warehouse":cur_frm.doc.source_warehouse}, ["actual_qty","valuation_rate"]).then((r)=>{
+														if(r.message.actual_qty){
+															recipe_item.available_qty = r.message.actual_qty;
+														}
+														if(r.message.valuation_rate){
+															let price = 0;
+															price = r.message.valuation_rate
+															recipe_item.rate = price;
+															recipe_item.amount = b.items[j].qty * price;
+														}
+													cur_frm.refresh_field("recipe_items");
+													})
+													recipe_item.parent_item = d.items[i].item_code;
+													recipe_item.bom = values.name;
+													recipe_item.material_request_ref = material_requests[s].material_request;
+												}
+										cur_frm.refresh_field("recipe_items");
+										})
+								})
+							}		
+					})
+				}
 			}
 		}
 		else{
-			let material_requests = cur_frm.doc.material_requests;
-			for(let s=0; s<material_requests.length; s++){
-			frappe.db.get_doc('Material Request', material_requests[s].material_request)
-    			.then(d => {
-        				//console.log(d.items);
-						for (let i = 0; i < d.items.length; i++){
-							let main_item = cur_frm.add_child("main_items");
-							main_item.item_code = d.items[i].item_code;
-							main_item.item_name = d.items[i].item_name;
-							main_item.qty = d.items[i].qty;
-							main_item.uom = d.items[i].uom;
-							main_item.rate = d.items[i].rate;
-							main_item.amount = d.items[i].amount;
-							main_item.reference_doc = material_requests[s].material_request
-							cur_frm.refresh_field("main_items");
-
-							frappe.db.get_value('BOM', {item: d.items[i].item_code, is_default: '1'}, ['name','total_cost'])
-    						.then(r => {
-        							let values = r.message;
-        							//console.log(values);
-									let t_bom = cur_frm.add_child("bom_list");
-									t_bom.bom = values.name;
-									t_bom.cost_per_unit = values.total_cost
-									t_bom.reference_doc = material_requests[s].material_request
-									t_bom.main_item = d.items[i].item_code;
-									cur_frm.refresh_field("bom_list");
-
-									frappe.db.get_doc('BOM', values.name)
-    									.then(b => {
-        									//console.log(b.items);
-											for (let j = 0; j < b.items.length; j++){
-												let recipe_item = cur_frm.add_child("recipe_items");
-												recipe_item.item_code = b.items[j].item_code;
-												recipe_item.item_name = b.items[j].item_name;
-												recipe_item.qty = b.items[j].qty;
-												recipe_item.uom = b.items[j].uom;
-												frappe.db.get_value("Bin", {"item_code":b.items[j].item_code, "warehouse":cur_frm.doc.source_warehouse}, "actual_qty").then((r)=>{
-													if(r.message.actual_qty){
-														recipe_item.available_qty = r.message.actual_qty;
-													}
-												cur_frm.refresh_field("recipe_items");
-												})
-												frappe.db.get_value("Bin", {"item_code":b.items[j].item_code}, "valuation_rate").then((r)=>{
-													if(r.message.valuation_rate){
-														let price = 0;
-														price = r.message.valuation_rate
-														recipe_item.rate = price;
-														recipe_item.amount = b.items[j].qty * price;
-													}
-												cur_frm.refresh_field("recipe_items");
-												})
-												recipe_item.parent_item = d.items[i].item_code;
-												recipe_item.bom = values.name;
-												recipe_item.reference_doc = material_requests[s].material_request
-											}
-									cur_frm.refresh_field("recipe_items");
-    								})
-    						})
-						}		
-    			})
-			}
+			frappe.msgprint(__('Enter Source and Target Warehouse '));
 		}
+		
 	}
 
 });
@@ -202,13 +208,14 @@ frappe.ui.form.on('MP Main Items', {
 				}
 			})
 
-			frappe.db.get_value('BOM', {item: row.item_code, is_default: '1'}, 'name')
+			frappe.db.get_value('BOM', {item: row.item_code, is_default: '1'}, ['name','total_cost'])
     		.then(r => {
         		let values = r.message;
         		//console.log(values.name);
 				let t_bom = cur_frm.add_child("bom_list");
 				t_bom.bom = values.name;
 				t_bom.main_item = row.item_code;
+				t_bom.cost_per_unit = values.total_cost;
 				cur_frm.refresh_field("bom_list");
 
 				frappe.db.get_doc('BOM', values.name)
@@ -220,14 +227,17 @@ frappe.ui.form.on('MP Main Items', {
 							recipe_item.item_name = d.items[i].item_name;
 							recipe_item.qty = d.items[i].qty;
 							recipe_item.uom = d.items[i].uom;
-							frappe.db.get_value("Bin", {"item_code":d.items[i].item_code}, "valuation_rate").then((r)=>{
-								if(r.message.valuation_rate){
-										let price = 0;
-										price = r.message.valuation_rate
-										recipe_item.rate = price;
-										recipe_item.amount = d.items[i].qty * price;
+							frappe.db.get_value("Bin", {"item_code":d.items[i].item_code, "warehouse":cur_frm.doc.source_warehouse}, ["actual_qty","valuation_rate"]).then((r)=>{
+								if(r.message.actual_qty){
+									recipe_item.available_qty = r.message.actual_qty;
 								}
-								cur_frm.refresh_field("recipe_items");
+								if(r.message.valuation_rate){
+									let price = 0;
+									price = r.message.valuation_rate
+									recipe_item.rate = price;
+									recipe_item.amount = d.items[i].qty * price;
+								}
+							cur_frm.refresh_field("recipe_items");
 							})
 							recipe_item.parent_item = row.item_code;
 							recipe_item.bom = values.name;
@@ -243,5 +253,84 @@ frappe.ui.form.on('MP Main Items', {
 		let row = locals[cdt][cdn];
 		row.amount = row.qty * row.rate;
 		frm.refresh_field("main_items");
+	},
+	before_main_items_remove:function(frm, cdt, cdn){
+		var deleted_row = frappe.get_doc(cdt, cdn);
+    	//console.log(deleted_row.item_code);
+		if(cur_frm.doc.bom_list && cur_frm.doc.recipe_items){
+			let boms = cur_frm.doc.bom_list;
+			for(var i=0;i<boms.length;i++){
+				if (boms[i].main_item == deleted_row.item_code){
+					cur_frm.get_field("bom_list").grid.grid_rows[i].remove();
+				}
+			}
+			let recipes = cur_frm.doc.recipe_items;
+			for(var j=recipes.length-1;j>=0;j--){
+				if(recipes[j].parent_item == deleted_row.item_code){
+					cur_frm.get_field("recipe_items").grid.grid_rows[j].remove();
+				}
+			}
+
+		}
+		
+		cur_frm.refresh();
 	}
-})
+});
+frappe.ui.form.on('Production Plan Sales Order', {
+	before_sales_orders_remove:function(frm, cdt, cdn){
+		var deleted_row = frappe.get_doc(cdt, cdn);
+    	//console.log(deleted_row.item_code);
+		if(cur_frm.doc.main_items && cur_frm.doc.bom_list && cur_frm.doc.recipe_items){
+			let m_items = cur_frm.doc.main_items;
+			for(var j=m_items.length-1;j>=0;j--){
+				if(m_items[j].sales_order_ref == deleted_row.sales_order){
+					cur_frm.get_field("main_items").grid.grid_rows[j].remove();
+				}
+			}
+			let boms = cur_frm.doc.bom_list;
+			for(var i=0;i<boms.length;i++){
+				if (boms[i].sales_order_ref == deleted_row.sales_order){
+					cur_frm.get_field("bom_list").grid.grid_rows[i].remove();
+				}
+			}
+			let recipes = cur_frm.doc.recipe_items;
+			for(var j=recipes.length-1;j>=0;j--){
+				if(recipes[j].sales_order_ref == deleted_row.sales_order){
+					cur_frm.get_field("recipe_items").grid.grid_rows[j].remove();
+				}
+			}
+
+		}
+		
+		cur_frm.refresh();
+	}
+});
+frappe.ui.form.on('Production Plan Material Request', {
+	before_material_requests_remove:function(frm, cdt, cdn){
+		var deleted_row = frappe.get_doc(cdt, cdn);
+    	//console.log(deleted_row.item_code);
+		if(cur_frm.doc.main_items && cur_frm.doc.bom_list && cur_frm.doc.recipe_items){
+			let m_items = cur_frm.doc.main_items;
+			for(var j=m_items.length-1;j>=0;j--){
+				if(m_items[j].material_request_ref == deleted_row.material_request){
+					cur_frm.get_field("main_items").grid.grid_rows[j].remove();
+				}
+			}
+			let boms = cur_frm.doc.bom_list;
+			for(var i=0;i<boms.length;i++){
+				if (boms[i].material_request_ref == deleted_row.material_request){
+					cur_frm.get_field("bom_list").grid.grid_rows[i].remove();
+				}
+			}
+			let recipes = cur_frm.doc.recipe_items;
+			for(var j=recipes.length-1;j>=0;j--){
+				if(recipes[j].material_request_ref == deleted_row.material_request){
+					cur_frm.get_field("recipe_items").grid.grid_rows[j].remove();
+				}
+			}
+
+		}
+		
+		cur_frm.refresh();
+	}
+});
--- kitchen_module/kitchen_module/doctype/meal_process/meal_process.py
+++ kitchen_module/kitchen_module/doctype/meal_process/meal_process.py
@@ -6,38 +6,127 @@ from frappe.model.document import Document
 
 class MealProcess(Document):
 	def before_save(self):
-		for i in self.main_items:
-			cost = 0
-			for j in self.recipe_items:
-				if i.item_code == j.parent_item and i.reference_doc == j.reference_doc:
-					if i.qty > 1:
-						j.qty = j.qty * i.qty
-						j.amount = j.qty * j.rate
-						cost = cost + j.amount
-					else:
-						cost = cost + j.amount	
-			i.cost = cost
+		if self.get_items_from == "Sales Order":
+			for i in self.main_items:
+				cost = 0
+				for j in self.recipe_items:
+					if i.item_code == j.parent_item and i.seles_order_ref == j.sales_order_ref:
+						if i.qty > 1:
+							j.qty = j.qty * i.qty
+							if j.rate:
+								j.amount = j.qty * j.rate
+								cost = cost + j.amount
+						else:
+							cost = cost + j.amount	
+				i.cost = cost
+				
+		elif self.get_items_from == "Material Request":
+			for i in self.main_items:
+				cost = 0
+				for j in self.recipe_items:
+					if i.item_code == j.parent_item and i.material_request_ref == j.material_request_ref:
+						if i.qty > 1:
+							j.qty = j.qty * i.qty
+							if j.rate:
+								j.amount = j.qty * j.rate
+								cost = cost + j.amount
+						else:
+							cost = cost + j.amount	
+				i.cost = cost
+				
 
-	def on_submit(self):
-		se = frappe.new_doc("Stock Entry")
-		se.stock_entry_type = "Manufacture"
-		se.meal_process = self.name
-		for i in self.recipe_items:
-			se_item = frappe.new_doc("Stock Entry Detail")
-			se_item.s_warehouse = self.source_warehouse
-			se_item.item_code = i.item_code
-			se_item.qty = i.qty
-			se_item.basic_rate = i.rate
-			se.append("items", se_item)
+	def before_submit(self):
+		if self.get_items_from == "Sales Order":
+			for i in self.main_items:
+				se = frappe.new_doc("Stock Entry")
+				se.stock_entry_type = "Manufacture"
+				se.meal_process = self.name
+				se.from_bom = 1
+				for j in self.bom_list:
+					if i.item_code == j.main_item and i.sales_order_ref == j.sales_order_ref:
+						se.bom_no = j.bom
+				se.fg_completed_qty = i.qty
 
-		for i in self.main_items:
-			se_item = frappe.new_doc("Stock Entry Detail")
-			se_item.t_warehouse = self.target_warehouse
-			se_item.item_code = i.item_code
-			se_item.qty = i.qty
-			se_item.basic_rate = i.cost
-			se.append("items", se_item)
+				for k in self.recipe_items:
+					if i.item_code == k.parent_item and i.sales_order_ref == k.sales_order_ref:
+						se_item = frappe.new_doc("Stock Entry Detail")
+						se_item.s_warehouse = self.source_warehouse
+						se_item.item_code = k.item_code
+						se_item.qty = k.qty
+						se_item.basic_rate = k.rate
+						if i.rate == 0:
+							se_item.allow_zero_valuation_rate = 1
+						se.append("items", se_item)
 
-		se.save()
-		se.submit()		
+				se_item = frappe.new_doc("Stock Entry Detail")
+				se_item.t_warehouse = self.target_warehouse
+				se_item.item_code = i.item_code
+				se_item.is_finished_item = 1
+				se_item.qty = i.qty
+				se_item.basic_rate = i.cost/i.qty
+				se.append("items", se_item)		
+
+				se.save()
+				se.submit() 
+
+		elif self.get_items_from == "Material Request":	
+			for i in self.main_items:
+				se = frappe.new_doc("Stock Entry")
+				se.stock_entry_type = "Manufacture"
+				se.meal_process = self.name
+				se.from_bom = 1
+				for j in self.bom_list:
+					if i.item_code == j.main_item and i.material_request_ref == j.material_request_ref:
+						se.bom_no = j.bom
+				se.fg_completed_qty = i.qty
+
+				for k in self.recipe_items:
+					if i.item_code == k.parent_item and i.material_request_ref == k.material_request_ref:
+						se_item = frappe.new_doc("Stock Entry Detail")
+						se_item.s_warehouse = self.source_warehouse
+						se_item.item_code = k.item_code
+						se_item.qty = k.qty
+						se_item.basic_rate = k.rate
+						if i.rate == 0:
+							se_item.allow_zero_valuation_rate = 1
+						se.append("items", se_item)
+
+				se_item = frappe.new_doc("Stock Entry Detail")
+				se_item.t_warehouse = self.target_warehouse
+				se_item.item_code = i.item_code
+				se_item.is_finished_item = 1
+				se_item.qty = i.qty
+				se_item.basic_rate = i.cost/i.qty
+				se.append("items", se_item)		
+
+				se.save()
+				se.submit()
+
+
+
+		# se = frappe.new_doc("Stock Entry")
+		# se.stock_entry_type = "Manufacture"
+		# se.meal_process = self.name
+		# for i in self.recipe_items:
+		# 	se_item = frappe.new_doc("Stock Entry Detail")
+		# 	se_item.s_warehouse = self.source_warehouse
+		# 	se_item.item_code = i.item_code
+		# 	se_item.qty = i.qty
+		# 	se_item.basic_rate = i.rate
+		# 	if i.rate == 0:
+		# 		se_item.allow_zero_valuation_rate = 1
+		# 	se.append("items", se_item)
+
+		# for j in self.main_items:
+		# 	se_item = frappe.new_doc("Stock Entry Detail")
+		# 	se_item.t_warehouse = self.target_warehouse
+		# 	se_item.item_code = j.item_code
+		# 	se_item.is_finished_item = 1
+		# 	se_item.qty = j.qty
+		# 	rate = j.cost/j.qty
+		# 	se_item.basic_rate = rate
+		# 	se.append("items", se_item)
+
+		# se.save()
+		# se.submit()
 
